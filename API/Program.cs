da4181efca4061926f19ba900f6250dae910dc11
buraq falah <buraqfalah1@gmail.com>
2024-02-24 11:31:02 +0800

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
@@ -29,4 +32,17 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context =services.GetRequiredService<StoreContext>();
+var logger =services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (System.Exception ex)
+{
+    logger.LogError(ex , "An error occured during migration");
+}
 app.Run();
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,4 @@
-
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,8 +8,15 @@ namespace Infrastructure.Data
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
-        }
-        // we need to tell the context that we want to map the StoreItems table
+
+        } 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
